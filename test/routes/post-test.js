b47571597ad08be3d9367dda95d53669a59a49ad
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-03-28 08:06:32 +0200

    Updates dependencies

--- test/routes/post-test.js
+++ test/routes/post-test.js
@@ -15,7 +15,6 @@ const getUrl = fn => {
 test('it returns result if text is POSTED', async t => {
   const url = await getUrl(srv)
   const result = await axios.post(url, data)
-
   t.truthy(result.data.text, 'POST ok - text returned')
   t.truthy(result.data.lix, 'POST ok - lix returned')
   t.truthy(result.data.kanselli, 'POST ok - kanselli returned')
